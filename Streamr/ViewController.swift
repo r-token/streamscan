5499aaf59647fdca00730ab2c65e19fd3f774a9f
Ryan Token <ryantoken13@icloud.com>
2019-04-16 22:59:32 -0500

    Initial Commit

---
+++ Streamr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Streamr
+//
+//  Created by Ryan Token on 4/16/19.
+//  Copyright © 2019 Ryan Token. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
